02669480b93b6c187b4b96b0e548381e046f50e2
Tomas Krejci <tomas@krej.ci>
2026-02-19 06:47:06 +0100

    Race condition.

--- client_folder/src/main.rs
+++ client_folder/src/main.rs
@@ -441,16 +441,16 @@ async fn start_file_sync(
                     let mut index = 0u32;
                     for d in diffs {
                         match d {
-                            diff::Result::Left(_) => {
-                                text.remove_range(&mut txn, index, 1);
+                            diff::Result::Left(c) => {
+                                text.remove_range(&mut txn, index, c.len_utf16() as u32);
                             }
                             diff::Result::Right(r) => {
                                 let s = r.to_string();
                                 text.insert(&mut txn, index, &s);
-                                index += 1;
+                                index += r.len_utf16() as u32;
                             }
-                            diff::Result::Both(_, _) => {
-                                index += 1;
+                            diff::Result::Both(c, _) => {
+                                index += c.len_utf16() as u32;
                             }
                         }
                     }
@@ -644,16 +644,16 @@ async fn sync_local_change(handler: &ActiveFile) -> anyhow::Result<()> {
 
     for d in diffs {
         match d {
-            diff::Result::Left(_) => {
-                text.remove_range(&mut txn, index, 1);
+            diff::Result::Left(c) => {
+                text.remove_range(&mut txn, index, c.len_utf16() as u32);
             }
             diff::Result::Right(r) => {
                 let s = r.to_string();
                 text.insert(&mut txn, index, &s);
-                index += 1;
+                index += r.len_utf16() as u32;
             }
-            diff::Result::Both(_, _) => {
-                index += 1;
+            diff::Result::Both(c, _) => {
+                index += c.len_utf16() as u32;
             }
         }
     }
